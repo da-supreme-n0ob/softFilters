b50934c5f19b285d6ad03dfacdb82c92deaebf82
da-supreme-n0ob <harrychess3@gmail.com>
2025-12-13 08:42:56 -0600

    finished

--- softFilterTest/src/highPassFilters/sharpeningFilter.java
+++ softFilterTest/src/highPassFilters/sharpeningFilter.java
@@ -1,4 +1,79 @@
 package highPassFilters;
 
+//filter for sharpening edges in a grayscale image
 public class sharpeningFilter {
+    //there are a few "intensities" of kernels
+    private final int[][] kernelNormal = {
+            {0,-1,0},
+            {-1,5,-1},
+            {0,-1,0}
+    };
+    private final int[][] kernelStrong = {
+            {-1,-1,-1},
+            {-1,9,-1},
+            {-1,-1,-1}
+    };
+    private final int[][] kernelWeak = {
+            {0,0,0},
+            {0,2,0},
+            {0,0,0}
+    };
+    private int[][] originalImage;
+    private int[][] sharpenedImage;
+
+    public sharpeningFilter(int[][] input, int width, int height,int intensity){
+        //intensity is a int from 0-2 and determines kernel used
+        this.originalImage=input;
+        switch(intensity){
+            case 0:
+                calculate(width,height,kernelNormal);
+                break;
+            case 1:
+                calculate(width,height,kernelStrong);
+                break;
+            case 2:
+                calculate(width,height,kernelWeak);
+                break;
+            default:
+                calculate(width,height,kernelNormal);
+                break;
+        }
+
+    }
+    public void calculate(int width,int height, int[][] kernel){
+        sharpenedImage=new int[height][width];
+        //applies kernel
+        for(int i=1;1<height-1;i++){//skips the borders
+            for(int j=1;j<width-1;j++){
+                int sum=0;
+                //applies kernels
+                for(int y=-1;y<=1;y++){
+                    for(int x=-1;x<=1;x++){
+                        sum+=originalImage[i+y][j+x]*kernel[y+1][x+1];
+                    }
+                }
+                //makes sure sum is in bounds
+                if(sum<0){
+                    sum=0;
+                }else if(sum>255){
+                    sum=255;
+                }
+                sharpenedImage[i][j]=sum;
+            }
+        }
+        //copies the remaining borders onto the output
+        //horizontal borders
+        for(int i=0;i<width;i++){
+            sharpenedImage[0][i]=originalImage[0][i];
+            sharpenedImage[height-1][i]=originalImage[height-1][i];
+        }
+        //vertical borders
+        for(int j=0;j<width;j++){
+            sharpenedImage[j][0]=originalImage[j][0];
+            sharpenedImage[j][width-1]=originalImage[j][width-1];
+        }
+    }
+    public int[][] getFilteredImage(){
+        return sharpenedImage;
+    }
 }
--- softFilterTest/src/highPassFilters/soberFilter.java
+++ softFilterTest/src/highPassFilters/soberFilter.java
@@ -1,4 +1,69 @@
 package highPassFilters;
-
+//edge detection
 public class soberFilter {
+
+    //standard sobel kernels
+    /*
+    * while i was researching there was a lot more
+    * different types of kernels used, but this was
+    * the most common
+    */
+    //kernel for vertical edges
+    private final int[][] sobelX={
+            {-1,0,1},
+            {-2,0,2},
+            {-1,0,1},
+    };
+    //kernel for horizontal edges
+    private final int[][] sobelY={
+            {-1,-2,-1},
+            {0,0,0},
+            {1,2,1},
+    };
+
+    private int rows;
+    private int columns;
+    private int[][] image;
+    private int[][] output;
+
+    public soberFilter(int[][] input, int height,int width){
+        this.image=new int[height][width];
+        this.output=calculate(height,width);
+    }
+    public int[][] calculate(int height,int width){
+        for(int i=0;i<height-1;i++){
+            for(int j=0;j<width-1;j++){
+                int x=applyKernel(image,i,j,sobelX);
+                int y=applyKernel(image,i,j,sobelX);
+
+                //gradient magnitude formula
+                int magnitude = (int) Math.sqrt(x*x+y*y);
+                //keeps magnitude in bound
+                if(magnitude<0){
+                    magnitude=0;
+                }else if(magnitude>255){
+                    magnitude=255;
+                }
+                output[i][j]=magnitude;
+            }
+        }
+        return output;
+    }
+    public int applyKernel(int[][] input, int x, int y, int[][] kernel){
+        int total=0;
+
+        //loops through a 3x3 "neighborhood"
+        for(int i=-1;i<=1;i++){
+            for(int j=-1;j<=1;j++){
+                int pixel = image[y+i][x+j];
+                int kernelValue = kernel[i+1][j+1];
+                total+=pixel*kernelValue;
+            }
+        }
+
+        return total;
+    }
+    public int[][] getOutput(){
+        return output;
+    }
 }
